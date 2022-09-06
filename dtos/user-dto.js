500c04f512934643b42822fd94dad27d91da87bc
diyor-git <85790219+diyor-git@users.noreply.github.com>
2022-09-06 19:51:00 +0500

    Update user-dto.js

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -2,10 +2,12 @@ module.exports = class UserDto {
   email;
   id;
   role;
+  cafe_id;
 
   constructor(model) {
     this.email = model.email;
     this.id = model._id;
     this.role = model.role;
+    this.cafe_id = model.cafe_id;
   }
 };
